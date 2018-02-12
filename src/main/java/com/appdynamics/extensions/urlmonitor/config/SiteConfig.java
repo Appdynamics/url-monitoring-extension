ea94eab1267080c171f0d052d769554dafba9d0d
BalakrishnaV <balakrishna.v@appdynamics.com>
2018-02-12 23:14:29 +0530

    v1.2.9 Updated License

--- src/main/java/com/appdynamics/extensions/urlmonitor/config/SiteConfig.java
+++ src/main/java/com/appdynamics/extensions/urlmonitor/config/SiteConfig.java
@@ -1,4 +1,11 @@
-    package com.appdynamics.extensions.urlmonitor.config;
+    /*
+ * Copyright 2014. AppDynamics LLC and its affiliates.
+ * All Rights Reserved.
+ * This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ * The copyright notice above does not evidence any actual or intended publication of such source code.
+ */
+
+package com.appdynamics.extensions.urlmonitor.config;
 
 import com.appdynamics.extensions.urlmonitor.auth.AuthTypeEnum;
 
